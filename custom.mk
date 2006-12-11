78e95d292255a9f4167556f3eb6df513c4405500
drhanson <drhanson@57cc3b79-4724-0410-8027-2bad1ea49a6c>
2006-12-11 00:41:36 +0000

    Initial revision drh | 1997-08-12 10:25:59 -0700 (Tue, 12 Aug 1997)

---
+++ custom.mk
@@ -0,0 +1,19 @@
+DIRSEPSTR=/
+O=.obj
+E=.exe
+CC=cc
+CFLAGS=-g
+LDFLAGS=-g
+YFLAGS=
+FILES=COPYRIGHT README LOG iburg.h iburg.c gram.y gram.c iburg.1 makefile
+
+all:		iburg$E
+
+gram.c:		gram.y;		byacc $(YFLAGS) -o $@ $^
+
+iburg.zip:	$(FILES)
+		zip $@ $^ *.ps sample*.brg
+		d=`pwd`; cd //atr/users/drh/pkg/iburg; zip $$d/$@ RCS/*.[chy1],v RCS/sample*
+
+clobber::	clean
+		rm -r y.tab.c gram.c iburg.zip *.ilk *.pdb
